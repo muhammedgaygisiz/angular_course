25875751a45cac59054788ae656cd7e9349b9277
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-19 19:41:08 +0100

    LifeCycle Methods

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,16 +1,66 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+ } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+OnInit,
+OnChanges,
+DoCheck,
+AfterContentInit,
+AfterContentChecked,
+AfterViewInit,
+AfterViewChecked,
+OnDestroy {
+
+  constructor() {
+    console.log('constructor called!');
+  }
+
   @Input() element: { type: string, name: string, content: string };
 
-  constructor() { }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy called');
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked called');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked called');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck called!');
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit() {
+    console.log('ngOnInit called!');
   }
 
 }
