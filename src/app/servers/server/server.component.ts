915db96daff493e5b369e17bbbc66a70def298bf
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-22 14:44:29 +0100

    Pass dynamic data to route (via dummy resolver)

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router, Data } from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -16,11 +16,18 @@ export class ServerComponent implements OnInit {
     private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.setServerById(id);
-    this.route.params.subscribe(
-      (params: Params) => {
-        this.setServerById(+params['id']);
+    // const id = +this.route.snapshot.params['id'];
+    // this.setServerById(id);
+    // this.route.params.subscribe(
+    //   (params: Params) => {
+    //     this.setServerById(+params['id']);
+    //   }
+    // );
+
+    this.route.data
+    .subscribe(
+      (data: Data) => {
+        this.server = data['server'];
       }
     );
   }
