99bb329d368222ceba8f628cf1a769a71bba7eb6
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-22 11:20:45 +0100

    Add Redirecting and Wildcard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,16 +11,27 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'users', component: UsersComponent, children: [
-    { path: ':id/:name', component: UserComponent },
-  ] },
-  { path: 'servers', component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
-    { path: ':id/edit', component: EditServerComponent }
-  ] },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':id/:name', component: UserComponent },
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':id', component: ServerComponent },
+      { path: ':id/edit', component: EditServerComponent }
+    ]
+  },
+  {
+    path: 'not-found', component: PageNotFoundComponent
+  },
+  {
+    path: 'something', redirectTo: '/not-found'
+  }
 ];
 
 @NgModule({
@@ -31,7 +42,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
