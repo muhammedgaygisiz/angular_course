b4eb2b308ae25db478ee21fe6ffc54660768fd40
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-23 16:15:32 +0100

    Prepare for reactive approach Connect FormGroup and form in html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
@@ -11,7 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
