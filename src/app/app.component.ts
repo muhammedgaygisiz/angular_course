768b8142e04fbc65a3197fa915441716e1e762ce
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-24 20:44:37 +0100

    Add spinner p

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(
     private http: HttpClient
@@ -45,6 +46,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{[key: string]: Post }>('https://angular-course-370fd.firebaseio.com/posts.json')
       .pipe(
@@ -61,6 +63,7 @@ export class AppComponent implements OnInit {
       )
       .subscribe(
         posts => {
+          this.isFetching = false;
           this.loadedPosts = posts;
         }
       );
