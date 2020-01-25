d2c73526a30c1aa14237eb559f50b27d5a53c9f8
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-25 11:44:45 +0100

    Implement store data into db

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,25 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
 
-    constructor(http: HttpClient) {}
+    constructor(
+        private http: HttpClient,
+        private recipeService: RecipeService
+        ) {}
 
-    
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.http.put(
+            'https://angular-course-370fd.firebaseio.com/recipes.json',
+            recipes,
+        )
+        .subscribe(
+            response => {
+                console.log(response);
+            }
+        );
+    }
 }
