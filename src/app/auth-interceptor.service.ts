7f46b76d1f7ffccb599a74f4bbd05e0cc56032ee
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-25 11:11:57 +0100

    Add second interceptor

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -13,20 +13,11 @@ export class AuthInterceptorService implements HttpInterceptor {
         req: HttpRequest<any>,
         next: HttpHandler
     ): Observable<HttpEvent<any>> {
-        console.log('Request is on its way');
-
         const modifiedRequest = req.clone({
             headers: req.headers.append('Auth', 'xyz')
         });
 
-        return next.handle(modifiedRequest)
-            .pipe(tap(
-                event => {
-                    if (event.type === HttpEventType.Response) {
-                        console.log('My response body: ', event.body);
-                    }
-                }
-            ));
+        return next.handle(modifiedRequest);
     }
 
 }
