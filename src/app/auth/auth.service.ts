4288ccfc61283765f1efdb77981d29fbddcd9cd3
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-26 13:32:03 +0100

    Fetch data with token

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { catchError, tap } from 'rxjs/operators';
-import { throwError, Subject } from 'rxjs';
+import { throwError, Subject, BehaviorSubject } from 'rxjs';
 
 import { User } from '../user.model';
 
@@ -19,7 +19,7 @@ export interface AuthResponseData {
 export class AuthService {
     private webApiKey = '...';
 
-    user = new Subject<User>();
+    user = new BehaviorSubject<User>(null);
 
     constructor(
         private http: HttpClient
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,15 +1,17 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, tap } from 'rxjs/operators';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
 
     constructor(
         private http: HttpClient,
-        private recipeService: RecipeService
+        private recipeService: RecipeService,
+        private authService: AuthService
     ) { }
 
     storeRecipes() {
@@ -27,30 +29,36 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.http
-            .get<Recipe[]>(
-                'https://angular-course-370fd.firebaseio.com/recipes.json'
+        return this.authService.user.pipe(
+            take(1),
+            exhaustMap(user => {
+                return this.http
+                    .get<Recipe[]>(
+                        'https://angular-course-370fd.firebaseio.com/recipes.json',
+                        {
+                            params: new HttpParams().set('auth', user.token)
+                        }
+                    );
+            }),
+            map(
+                recipes => {
+                    return recipes.map(
+                        recipe => {
+                            return {
+                                ...recipe,
+                                ingredients: recipe.ingredients
+                                    ? recipe.ingredients
+                                    : []
+                            };
+                        }
+                    );
+                }
+            ),
+            tap(
+                recipes => {
+                    this.recipeService.setRecipes(recipes);
+                }
             )
-            .pipe(
-                map(
-                    recipes => {
-                        return recipes.map(
-                            recipe => {
-                                return {
-                                    ...recipe,
-                                    ingredients: recipe.ingredients
-                                        ? recipe.ingredients
-                                        : []
-                                };
-                            }
-                        );
-                    }
-                ),
-                tap(
-                    recipes => {
-                        this.recipeService.setRecipes(recipes);
-                    }
-                )
-            );
+        );
     }
 }
