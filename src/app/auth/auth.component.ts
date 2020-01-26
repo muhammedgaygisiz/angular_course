1f831c6933c57be2f9020dbb1e40495c0d1a15d9
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-26 13:17:15 +0100

    Respect auth status in header

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-auth',
@@ -14,7 +15,8 @@ export class AuthComponent {
     error: string = null;
 
     constructor(
-        private authService: AuthService
+        private authService: AuthService,
+        private router: Router
     ) { }
 
     onSwitchMode() {
@@ -43,6 +45,7 @@ export class AuthComponent {
             resData => {
                 console.log(resData);
                 this.isLoading = false;
+                this.router.navigate(['/recipes']);
             },
             errorMessage => {
                 this.error = errorMessage;
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  userSub: Subscription;
 
   constructor(
-    private dataStorageServcie: DataStorageService
+    private dataStorageServcie: DataStorageService,
+    private authService: AuthService
   ) {}
 
   ngOnInit() {
+    this.userSub = this.authService.user.subscribe(
+      user => {
+        this.isAuthenticated = !!user;
+      }
+    );
   }
 
   onSaveData() {
@@ -22,5 +32,9 @@ export class HeaderComponent implements OnInit {
 
   onFetchData() {
     this.dataStorageServcie.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 }
