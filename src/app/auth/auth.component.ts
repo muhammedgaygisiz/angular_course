7f721803b2900097f949818293b23d59bd270e27
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-26 20:26:16 +0100

    Create component programmatically and attach

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ComponentFactoryResolver, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 import { AuthService, AuthResponseData } from './auth.service';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
+import { AlertComponent } from '../shared/alert/alert.component';
+import { PlaceholderDirective } from '../shared/placeholder/placeholder.directive';
 
 @Component({
     selector: 'app-auth',
@@ -13,10 +15,12 @@ export class AuthComponent {
     isLoginMode = true;
     isLoading = false;
     error: string = null;
+    @ViewChild(PlaceholderDirective, { static: false }) alertHost: PlaceholderDirective;
 
     constructor(
         private authService: AuthService,
-        private router: Router
+        private router: Router,
+        private componentFactoryResolver: ComponentFactoryResolver
     ) { }
 
     onSwitchMode() {
@@ -48,7 +52,9 @@ export class AuthComponent {
                 this.router.navigate(['/recipes']);
             },
             errorMessage => {
+                console.log(errorMessage);
                 this.error = errorMessage;
+                this.showErrorAlert(errorMessage);
                 this.isLoading = false;
             }
         );
@@ -59,5 +65,15 @@ export class AuthComponent {
 
     onHandleError() {
         this.error = null;
+    }
+
+    private showErrorAlert(message: string) {
+        const alertCmpFactory = this.componentFactoryResolver
+            .resolveComponentFactory(AlertComponent);
+
+        const hostViewContainerRef = this.alertHost.viewContainerRef;
+        hostViewContainerRef.clear();
+
+        hostViewContainerRef.createComponent(alertCmpFactory);
     }
 }
