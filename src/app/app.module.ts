3389f700a9d06a767e81bf591a69e8b18b5f5fb8
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-27 08:56:29 +0100

    Another example of app-wide service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CoreModule } from './core.module';
 import { HeaderComponent } from './header/header.component';
 import { SharedModule } from './shared/shared.module';
+import { LoggingService } from './logging.service';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,11 @@ import { SharedModule } from './shared/shared.module';
     SharedModule,
     CoreModule,
   ],
-  bootstrap: [AppComponent],
+  bootstrap: [
+    AppComponent
+  ],
+  providers: [
+    LoggingService
+  ]
 })
 export class AppModule { }
