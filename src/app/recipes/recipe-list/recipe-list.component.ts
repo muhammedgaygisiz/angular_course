7b67b35f2c95130b8def149a9196d4fa9ab25843
Muhammed Gaygisiz <muhammed.gaygisiz@gmail.com>
2020-01-28 15:45:23 +0100

    Basic animation between two states

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
@@ -10,8 +11,20 @@ import { Recipe } from '../recipe.model';
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
+  animations: [
+    trigger('buttonState', [
+      state('normal', style({})),
+      state('activated', style({
+        backgroundColor: '#337ab7',
+        borderColor: '#2e6da4'
+      })),
+      transition('normal => activated', animate(300)),
+      transition('activated => normal', animate(300))
+    ])
+  ]
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
+  state = 'normal';
   recipes: Recipe[];
   subscription: Subscription;
 
@@ -34,6 +47,7 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   }
 
   onNewRecipe() {
+    this.state === 'normal' ? this.state = 'activated' : this.state = 'normal';
     this.router.navigate(['new'], { relativeTo: this.route });
   }
 
